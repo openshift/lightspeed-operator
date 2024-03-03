a102895711e99c640d442736ad500933b899ea56
xavier <xavier@redhat.com>
2024-03-02 21:51:01 -0500

    bundle and catalog builds
    Signed-off-by: xavier <xavier@redhat.com>
    
     updated version for bundle
    
    Signed-off-by: xavier <xavier@redhat.com>
    
    Updated per review comments
    
    Signed-off-by: xavier <xavier@redhat.com>
    
    updated example file
    
    Signed-off-by: xavier <xavier@redhat.com>
    
    updated example file
    
    Signed-off-by: xavier <xavier@redhat.com>
    
    updated script
    
    Signed-off-by: xavier <xavier@redhat.com>

---
+++ hack/build_catalog.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -euo pipefail
+DEFAULT_PLATFORM="linux/amd64"
+export QUAY_USER=""  # Set quay user for personal builds
+VERSION="0.0.1"    # Set the version
+QUAY_USER="${QUAY_USER:-openshift}"
+
+export IMAGE_TAG_BASE="quay.io/${QUAY_USER}/lightspeed-operator"
+BUNDLE_IMAGE="quay.io/${QUAY_USER}/lightspeed-operator-bundle:v${VERSION}"
+CATALOG_IMAGE="quay.io/${QUAY_USER}/lightspeed-catalog:${VERSION}"
+
+CSV_FILE="bundle/manifests/lightspeed-operator.clusterserviceversion.yaml"
+CATALOG_FILE="lightspeed-catalog/operator.yaml"
+CATALOG_DOCKER_FILE="lightspeed-catalog.Dockerfile"
+
+echo "Building File Based Catalog (FBC) for ${USER}"
+echo "Set IMAGE_TAG_BASE: ${IMAGE_TAG_BASE}"
+
+rm -rf ./bundle
+make bundle VERSION="${VERSION}"
+sed -i.bak "s/name: lightspeed-operator.v[0-9]+.[0-9]+.[0-9]+/name: lightspeed-operator.v${VERSION}/" "${CSV_FILE}"
+sed -i.bak "s/version: [0-9]+.[0-9]+.[0-9]+/version: ${VERSION}/" "${CSV_FILE}"
+rm -rf "${CSV_FILE}.bak"
+echo "Built bundle image ${BUNDLE_IMAGE}"
+
+make bundle-build bundle-push VERSION="${VERSION}" BUNDLE_IMAGE="${BUNDLE_IMAGE}"
+
+echo "Adding bundle image to FBC using image ${BUNDLE_IMAGE}" 
+if [ -f "${CATALOG_FILE}" ]; then
+  rm -f "${CATALOG_FILE}"
+fi
+touch "${CATALOG_FILE}"
+cat << EOF >> "${CATALOG_FILE}"
+---
+defaultChannel: preview
+name: lightspeed-operator
+schema: olm.package
+EOF
+
+bin/opm render "${BUNDLE_IMAGE}" --output=yaml >> "${CATALOG_FILE}"
+cat << EOF >> "${CATALOG_FILE}"
+---
+schema: olm.channel
+package: lightspeed-operator
+name: preview
+entries:
+  - name: lightspeed-operator.v$VERSION
+EOF
+echo "Building catalog image ${CATALOG_IMAGE}"
+podman build --platform="${DEFAULT_PLATFORM}" -f "${CATALOG_DOCKER_FILE}" -t "${CATALOG_IMAGE}" .
+echo "Pushing catalog image ${CATALOG_IMAGE}"
+podman push "${CATALOG_IMAGE}"
