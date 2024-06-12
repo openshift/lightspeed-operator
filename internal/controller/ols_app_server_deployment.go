d71c8dbb59f44bd038c83f1c8cdddd280cb6a9e1
Ondrej Metelka <ometelka@redhat.com>
2024-06-12 14:58:28 +0200

    Lower collector container resources

--- internal/controller/ols_app_server_deployment.go
+++ internal/controller/ols_app_server_deployment.go
@@ -43,8 +43,8 @@ func getOLSDataCollectorResources(cr *olsv1alpha1.OLSConfig) *corev1.ResourceReq
 	}
 	// default resources.
 	defaultResources := &corev1.ResourceRequirements{
-		Limits:   corev1.ResourceList{corev1.ResourceCPU: resource.MustParse("100m"), corev1.ResourceMemory: resource.MustParse("512Mi")},
-		Requests: corev1.ResourceList{corev1.ResourceCPU: resource.MustParse("50m"), corev1.ResourceMemory: resource.MustParse("128Mi")},
+		Limits:   corev1.ResourceList{corev1.ResourceCPU: resource.MustParse("100m"), corev1.ResourceMemory: resource.MustParse("128Mi")},
+		Requests: corev1.ResourceList{corev1.ResourceCPU: resource.MustParse("50m"), corev1.ResourceMemory: resource.MustParse("64Mi")},
 		Claims:   []corev1.ResourceClaim{},
 	}
 
