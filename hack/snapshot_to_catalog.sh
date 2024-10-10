314f66e0b8c931dc6b42c65df30b6585e99bc0df
Haoyu Sun <hasun@redhat.com>
2024-10-10 11:10:41 +0200

    snapshot_to_catalog.sh: add migrate option for OCP 4.17+
    Signed-off-by: Haoyu Sun <hasun@redhat.com>

--- hack/snapshot_to_catalog.sh
+++ hack/snapshot_to_catalog.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 usage() {
-  echo "Usage: $0 -s <snapshot-refs> -c <catalog-file> -n <channel-names>"
-  echo "  snapshot-refs: required, the snapshots' references to use, ordered by versions ascending, example: ols-cq8sl,ols-mdc8x"
-  echo "  catalog-file: the catalog index file to update, default: lightspeed-catalog-4.16/index.yaml"
-  echo "  channel-names: the channel names to update, default: alpha"
+  echo "Usage: $0 -s <snapshot-refs> -c <catalog-file> -n <channel-names> -m"
+  echo "  -s snapshot-refs: required, the snapshots' references to use, ordered by versions ascending, example: ols-cq8sl,ols-mdc8x"
+  echo "  -c catalog-file: the catalog index file to update, default: lightspeed-catalog-4.16/index.yaml"
+  echo "  -n channel-names: the channel names to update, default: alpha"
+  echo "  -m migrate: migrate the bundle object to csv metadata, required for OCP 4.17+, default: false"
   echo "Example: $0 -s ols-cq8sl,ols-mdc8x -c lightspeed-catalog-4.16/index.yaml"
 }
 
@@ -13,11 +14,16 @@ if [ $# == 0 ]; then
   exit 1
 fi
 
+version_gt() {
+  test "$(printf '%s\n' "$@" | sort -V | tail -n 1)" != "$1"
+}
+
 SNAPSHOT_REFS=""
 CATALOG_FILE="lightspeed-catalog-4.16/index.yaml"
 CHANNEL_NAMES="alpha"
+MIGRATE=""
 
-while getopts ":s:c:n:h" argname; do
+while getopts ":s:c:n:mh" argname; do
   case "$argname" in
   "s")
     SNAPSHOT_REFS=${OPTARG}
@@ -27,6 +33,9 @@ while getopts ":s:c:n:h" argname; do
     ;;
   "n")
     CHANNEL_NAMES=${OPTARG}
+    ;;
+  "m")
+    MIGRATE="true"
     ;;
   "h")
     usage
@@ -44,6 +53,8 @@ while getopts ":s:c:n:h" argname; do
   esac
 done
 
+echo migrate ${MIGRATE}
+
 if [ -z "${SNAPSHOT_REFS}" ]; then
   echo "snapshot-refs is required"
   usage
@@ -55,9 +66,10 @@ CATALOG_INITIAL_FILE="${SCRIPT_DIR}/operator.yaml"
 
 : ${OPM:=$(command -v opm)}
 echo "using opm from ${OPM}"
-# check if opm version is v1.39.0 or exit
-if ! ${OPM} version | grep -q "v1.27.1"; then
-  echo "opm version v1.27.1 is required"
+# check if opm version is greater than v1.46.0 or exit
+OPM_VERSION=$(${OPM} version | grep -Eo 'OpmVersion:"[^"]*"' | grep -Eo '[0-9]*\.[0-9]*\.[0-9]*')
+if version_gt ${OPM_VERSION} 1.46.0; then
+  echo "opm version > 1.46.0 is required, current version is ${OPM_VERSION}"
   exit 1
 fi
 
@@ -151,7 +163,11 @@ EOF
   echo "SERVICE_IMAGE=${SERVICE_IMAGE}"
 
   echo BUNDLE_IMAGE_ORIGIN=${BUNDLE_IMAGE_ORIGIN}
-  ${OPM} render ${BUNDLE_IMAGE_ORIGIN} --output=yaml >"${TEMP_BUNDLE_FILE}"
+  OPM_ARGS=""
+  if [ -n "${MIGRATE}" ]; then
+    OPM_ARGS="--migrate-level=bundle-object-to-csv-metadata"
+  fi
+  ${OPM} render ${BUNDLE_IMAGE_ORIGIN} --output=yaml ${OPM_ARGS} >"${TEMP_BUNDLE_FILE}"
   BUNDLE_VERSION=$(yq '.properties[]| select(.type=="olm.package")| select(.value.packageName=="lightspeed-operator") |.value.version' ${TEMP_BUNDLE_FILE})
   echo "Bundle version is ${BUNDLE_VERSION}"
   BUNDLE_VERSIONS+=("${BUNDLE_VERSION}")
