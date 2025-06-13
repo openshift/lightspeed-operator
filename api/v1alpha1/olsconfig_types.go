b09e847beb81849a07607670e90ab1dc7b130607
Haoyu Sun <hasun@redhat.com>
2025-06-13 17:55:25 +0200

    bugfix: ols.defaultProvider should be required in UI
    Signed-off-by: Haoyu Sun <hasun@redhat.com>

--- api/v1alpha1/olsconfig_types.go
+++ api/v1alpha1/olsconfig_types.go
@@ -69,6 +69,8 @@ type OLSSpec struct {
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Default Model",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:text"}
 	DefaultModel string `json:"defaultModel"`
 	// Default provider for usage
+	// +kubebuilder:validation:Required
+	// +required
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Default Provider",xDescriptors={"urn:alm:descriptor:com.tectonic.ui:text"}
 	DefaultProvider string `json:"defaultProvider,omitempty"`
 	// Query filters
