ff548ec1f1855dcec40eefba23f69332ad7f51e5
Sergey Yedrikov <syedriko@redhat.com>
2025-07-25 17:26:15 -0400

    OLS-1915: added a BYOK e2e test

---
+++ test/e2e/byok_test.go
@@ -0,0 +1,57 @@
+package e2e
+
+import (
+	"fmt"
+	"net/http"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	olsv1alpha1 "github.com/openshift/lightspeed-operator/api/v1alpha1"
+)
+
+var _ = Describe("BYOK", Ordered, Label("BYOK"), func() {
+	var env *OLSTestEnvironment
+	var err error
+
+	BeforeAll(func() {
+		By("Setting up OLS test environment with RAG configuration")
+		env, err = SetupOLSTestEnvironment(func(cr *olsv1alpha1.OLSConfig) {
+			cr.Spec.OLSConfig.RAG = []olsv1alpha1.RAGSpec{
+				{
+					Image: "quay.io/openshift-lightspeed-test/assisted-installer-guide:2025-1",
+				},
+			}
+		})
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterAll(func() {
+		By("Cleaning up OLS test environment with CR deletion")
+		err = CleanupOLSTestEnvironmentWithCRDeletion(env, "byok_test")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should query the BYOK database", func() {
+		By("Testing OLS service activation")
+		secret, err := TestOLSServiceActivation(env)
+		Expect(err).NotTo(HaveOccurred())
+
+		By("Testing HTTPS POST on /v1/query endpoint by OLS user")
+		reqBody := []byte(`{"query": "what CPU architectures does the assisted installer support?"}`)
+		resp, body, err := TestHTTPSQueryEndpoint(env, secret, reqBody)
+		fmt.Println("httpsClient.PostJson", map[string]string{"Authorization": "Bearer " + env.SAToken})
+		Expect(err).NotTo(HaveOccurred())
+		defer resp.Body.Close()
+		Expect(resp.StatusCode).To(Equal(http.StatusOK))
+		fmt.Println(string(body))
+
+		Expect(string(body)).To(
+			And(
+				ContainSubstring("x86_64"),
+				ContainSubstring("arm64"),
+				ContainSubstring("ppc64le"),
+				ContainSubstring("s390x"),
+			),
+		)
+	})
+})
